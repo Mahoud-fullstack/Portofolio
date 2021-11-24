e42bd275b423e5fc05a5af827eac11ca940edcdf
Mahoud-fullstack <acc.msaaad@gmail.com>
2021-11-24 09:36:35 -0500

    Create new components for pages

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
+import Contact from './Contact';
+import Home from './Home';
+import Projects from './Projects';
 
 function App() {
-  return (
-    <div className="App">
-      
+  return  <div className="App">
+   <Home/>
+   <Projects/>
+   <Contact/>
     </div>
-  );
+  ;
 }
 
 export default App;
